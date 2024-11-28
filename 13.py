e0a835281bb2ca4db80503ec10d08b7aa8c0d6a7
Aditya Kartik <adityakartik43@gmail.com>
2024-11-28 23:04:59 +0530

    DAA-Lab

--- 13.py
+++ 13.py
@@ -1,23 +1,174 @@
-from sortedcontainers import SortedSet
+# from sortedcontainers import SortedSet
+
+# class RedBlackTree:
+#     def __init__(self):
+#         self.tree = SortedSet()
+
+#     def insert(self, value):
+#         self.tree.add(value)
+
+#     def delete(self, value):
+#         self.tree.discard(value)
+
+#     def __str__(self):
+#         return str(list(self.tree))
+
+# rb_tree = RedBlackTree()
+# rb_tree.insert(10)
+# rb_tree.insert(20)
+# rb_tree.delete(16)
+# rb_tree.delete(112)
+# rb_tree.delete(122)
+# print("Red-Black Tree:", rb_tree)
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.color = 'RED'
+        self.left = None
+        self.right = None
+        self.parent = None
+
 
 class RedBlackTree:
     def __init__(self):
-        self.tree = SortedSet()
+        self.TNULL = Node(0)
+        self.TNULL.color = 'BLACK'
+        self.root = self.TNULL
 
-    def insert(self, value):
-        self.tree.add(value)
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.TNULL:
+            y.left.parent = x
 
-    def delete(self, value):
-        self.tree.discard(value)
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
 
-    def __str__(self):
-        return str(list(self.tree))
+    def right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right != self.TNULL:
+            y.right.parent = x
 
-rb_tree = RedBlackTree()
-rb_tree.insert(10)
-rb_tree.insert(20)
-rb_tree.delete(16)
-rb_tree.delete(112)
-rb_tree.delete(122)
-print("Red-Black Tree:", rb_tree)
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
 
+    def fix_insert(self, k):
+        while k.parent.color == 'RED':
+            if k.parent == k.parent.parent.right:
+                u = k.parent.parent.left
+                if u.color == 'RED':
+                    u.color = 'BLACK'
+                    k.parent.color = 'BLACK'
+                    k.parent.parent.color = 'RED'
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.left:
+                        k = k.parent
+                        self.right_rotate(k)
+                    k.parent.color = 'BLACK'
+                    k.parent.parent.color = 'RED'
+                    self.left_rotate(k.parent.parent)
+            else:
+                u = k.parent.parent.right
+                if u.color == 'RED':
+                    u.color = 'BLACK'
+                    k.parent.color = 'BLACK'
+                    k.parent.parent.color = 'RED'
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.right:
+                        k = k.parent
+                        self.left_rotate(k)
+                    k.parent.color = 'BLACK'
+                    k.parent.parent.color = 'RED'
+                    self.right_rotate(k.parent.parent)
+            if k == self.root:
+                break
+        self.root.color = 'BLACK'
+
+    def insert(self, key):
+        node = Node(key)
+        node.parent = None
+        node.data = key
+        node.left = self.TNULL
+        node.right = self.TNULL
+        node.color = 'RED'
+
+        y = None
+        x = self.root
+
+        while x != self.TNULL:
+            y = x
+            if node.data < x.data:
+                x = x.left
+            else:
+                x = x.right
+
+        node.parent = y
+        if y is None:
+            self.root = node
+        elif node.data < y.data:
+            y.left = node
+        else:
+            y.right = node
+
+        if node.parent is None:
+            node.color = 'BLACK'
+            return
+
+        if node.parent.parent is None:
+            return
+
+        self.fix_insert(node)
+
+    def inorder_helper(self, node):
+        if node != self.TNULL:
+            self.inorder_helper(node.left)
+            print(f"{node.data} ({node.color})", end=" ")
+            self.inorder_helper(node.right)
+
+    def inorder(self):
+        self.inorder_helper(self.root)
+        print()
+
+
+# Example usage
+if __name__ == "__main__":
+    rbt = RedBlackTree()
+    rbt.insert(55)
+    rbt.insert(40)
+    rbt.insert(65)
+    rbt.insert(60)
+    rbt.insert(75)
+    rbt.insert(57)
+
+    print("Inorder Traversal of Created Tree:")
+    rbt.inorder()
